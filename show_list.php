6879e7e5c45839e05af392ce3412a0416b80f8c6
Alvin <toast1110@hotmail.com>
2011-01-18 21:00:29 +0800

    完成show_list
    Signed-off-by: Alvin <toast1110@hotmail.com>

--- show_list.php
+++ show_list.php
@@ -6,6 +6,50 @@
 </head>
 
 <body>
-	
+	<?php
+		$currday = date("Y-m-d");
+		
+		$db_host = 'localhost' ;
+		$db_database = 'DBproject' ;
+		$db_username = 'root' ;
+		$connection = mysql_connect($db_host, $db_username, '');
+		if (!$connection)
+			die ("connection failed".mysql_error()) ;
+		$selection = mysql_select_db($db_database) ;
+		if (!$selection)
+			die ("selection failed".mysql_error()) ;
+		mysql_query("SET NAMES 'utf8'");
+	?>
+    <h1>近期演出資訊</h1>
+    	<table>
+    	<tr>
+        	<td>表演名稱</td>
+            <td>日期</td>
+            <td>地點</td>
+            <td>售票系統</td>
+        </tr>
+        <?php
+			$show = "SELECT show.name, show.day, location.name, sellsystem.name  
+					 FROM `show`, `location`, `sellsystem`
+					 WHERE show.l_id=location.id AND show.sell_id=sellsystem.id
+					 ORDER BY show.day DESC;" ;
+			$show_result = mysql_query($show);
+			while ($result_row = mysql_fetch_row(($show_result)))
+			{
+//				if($result_row[1]>=$currday){
+					echo "<tr>" ;
+					echo "<td>".$result_row[0]."</td>" ;
+					echo "<td>".$result_row[1]."</td>" ;
+					echo "<td>".$result_row[2]."</td>" ;
+					echo "<td>".$result_row[3]."</td>" ;
+					echo "</tr>";
+//				}
+			}
+		?>
+        <tr>
+        	<td></td>
+        </tr>
+    </table>
 </body>
+
 </html>

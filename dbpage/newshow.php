ba2fa4e2e68157913920fbaca4c996ea483a1d29
nanmadol <nanmadol1019@gmail.com>
2011-01-19 01:11:22 +0800

    newshow1與newshow2頁面完成 都是dbpage中的newshow newshow2只要把幾個不需要得tr刪掉即可
    Signed-off-by: nanmadol <nanmadol1019@gmail.com>

---
+++ dbpage/newshow.php
@@ -0,0 +1,15 @@
+<?php require_once "include/header_logged_artist.php"; ?>
+
+	<table>
+    	<th colspan='2' class="head">新增表演</th>
+    	<tr><td class="left">表演名稱</td><td></td></tr>
+        <tr><td class="left">表演日期(yyyy/mm/dd)</td><td></td></tr>
+        <tr><td class="left">表演時間(hh:mm)</td><td></td></tr>
+        <tr><td class="left">表演地點</td><td></td></tr>
+        <tr><td class="left">表演主要風格</td><td></td></tr>
+        <tr><td class="left">表演樂器</td><td></td></tr>
+        <tr><td class="left">售票系統</td><td></td></tr>
+        <tr><td class="left">確認</td><td>清除</td></tr>
+    </table>
+
+<?php require_once "include/foot.php"; ?>

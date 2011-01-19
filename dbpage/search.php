4fc6e81326a0d3651341f51882534609dad4ad71
edwardga <edwardgapeng@gmail.com>
2011-01-19 16:16:25 +0800

    css addon search
    Signed-off-by: edwardga <edwardgapeng@gmail.com>

--- dbpage/search.php
+++ dbpage/search.php
@@ -1,14 +1,22 @@
-<?php
+<?php 
 	include_once('include/auth.php') ;
-	$auth = auth() ;
-	if ($auth == NULL)
-		echo "您沒有使用此頁面的權力" ;
+	if(auth()==NULL)
+	{
+		require_once "include/header_index.php";
+		echo "<h1>您沒有使用此頁面的權力</h1>" ;
+	}
 	else
 	{
+		if(auth()=="audience")
+			require_once "include/header_logged_aud.php";
+		elseif (auth()=="artist")
+			require_once "include/header_logged_artist.php";
 		echo"
+			</div>
+			<div class='indextable'>
 			<form method = 'get' action = 'search1.php'>
-			搜尋
 			<table>
+			<th colspan='2' class='head'>搜尋</th>
 				<tr>
 					<td>表演名稱</td><td><input name = 'name' type = 'text'></td>
 			    </tr>
@@ -21,9 +29,10 @@
 			    <tr>
 				    <td>風格</td><td><input name = 'style' type = 'text'></td>
 			    </tr>
+			<th colspan='2' class='foot'><input type='submit' name = 'search' value = '搜尋' ></th>
 			</table>
-			<input type='submit' name = 'search' value = '搜尋' >
 			</form>
 			" ;
 	}
+	require_once "include/foot.php";
 ?>

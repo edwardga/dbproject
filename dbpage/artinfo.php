c60d6cb5ff8423a3cc3e7ef068d2dc5dd3517ea7
edwardga <edwardgapeng@gmail.com>
2011-01-20 14:53:30 +0800

    add link to song info
    Signed-off-by: edwardga <edwardgapeng@gmail.com>

--- dbpage/artinfo.php
+++ dbpage/artinfo.php
@@ -21,13 +21,13 @@
 </div>
 	<div class = "indextable">
     <table>
-    		<tr>
+<tr>
             <th colspan="2" class="head"><?php echo $find[1]; ?></th>
         </tr>
         <tr>
         	<td>出道年份</td>
             <td><?php echo $find[2]; ?></td>
-        </tr>
+        </tr>        
         </table>
 
         

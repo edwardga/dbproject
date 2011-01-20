21a74ac41ef97c0916edd5feba53a03b22b56fdb
nanmadol <nanmadol1019@gmail.com>
2011-01-20 09:21:04 +0800

    guestbook.php css套用
    Signed-off-by: nanmadol <nanmadol1019@gmail.com>

--- dbpage/guestbook.php
+++ dbpage/guestbook.php
@@ -8,12 +8,29 @@
 		require_once "include/header_index.php";
 	mysql_query("SET NAMES 'utf8'");
 ?>
+</div>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 
 <body>
+<div class="indextable">
+<table>
+	<form method="post" action="message.php">
+	<tr>
+    	<td style="border-top-left-radius:10px">標題</td>
+        <td style="padding-left:20px; border-top-right-radius:10px"><input name = 'title' type = 'text'></td>
+    </tr>
+    <tr>
+    	<td>內容</td>
+        <td><textarea name = 'content' cols="40" rows="5" style="resize:none"></textarea></td>
+    </tr>
+    <th colspan="2" class="foot"><input name = 'submit' type = 'submit' value = '送出'></th>
+    </form>
+</table>
+</div>
+<div id="showmes">
 <table>
 <?php
 	$display1 = "SELECT * FROM guestbook ORDER BY time DESC" ;
@@ -41,22 +58,7 @@
 	}
 ?>
 </table>
-
-<table>
-	<form method="post" action="message.php">
-	<tr>
-    	<td>標題</td>
-        <td><input name = 'title' type = 'text'></td>
-    </tr>
-    <tr>
-    	<td>內容</td>
-        <td><textarea name = 'content' cols="40" rows="5" style="resize:none"></textarea></td>
-    </tr>
-    <tr>
-    	<td></td>
-        <td><input name = 'submit' type = 'submit' value = '送出'></td>
-    </tr>
-    </form>
-</table>
+</div>
 </body>
 </html>
+<?php require_once "include/foot.php"; ?>
